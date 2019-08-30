0d81d293e9a0b0eb970bcc984502d56f89719db2
Kai Vandivier <kpvandivier@gmail.com>
2019-08-30 16:42:07 +0200

    Make functioning delete button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// TODO: Make it look pretty
+// TODO: Make "read" toggles
 
 let library = []
+
 const newBookBtn = document.querySelector('#new-book')
 newBookBtn.addEventListener('click', displayNewBookForm)
+
 const addBookBtn = document.querySelector('#add-book')
 addBookBtn.addEventListener('click', addBookToLibrary)
 
-// "Constructor"
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -24,18 +27,16 @@ function displayNewBookForm() {
   btn.classList.toggle('hidden')
 }
 
-function addBookToLibrary() {
-  // TODO: Get user input
-  let newInfo = document.getElementById('new-book-form')
-
-  console.log(newInfo.elements)
-  console.log(newInfo.elements.newPages.valueAsNumber)
-
-  let newTitle = newInfo.elements.newTitle.value
-  let newAuthor = newInfo.elements.newAuthor.value 
-  let newPages = newInfo.elements.newPages.valueAsNumber 
-  let newRead = newInfo.elements.newRead.checked
-  let newBook = new Book(newTitle,newAuthor,newPages,newRead)
+function addBookToLibrary(newBook = null) {
+  // TODO: Validate form input
+  if (!newBook) {
+    let newInfo = document.getElementById('new-book-form')
+    let newTitle = newInfo.elements.newTitle.value
+    let newAuthor = newInfo.elements.newAuthor.value 
+    let newPages = newInfo.elements.newPages.valueAsNumber 
+    let newRead = newInfo.elements.newRead.checked
+    newBook = new Book(newTitle, newAuthor, newPages, newRead)
+  }
   library.push(newBook)
   renderBook(newBook)
   clearForm()
@@ -53,6 +54,8 @@ function clearForm() {
 
 function renderTable(library) {
   const table = document.querySelector('tbody');
+  const rows = table.querySelectorAll('tr')
+  rows.forEach((row) => table.removeChild(row)) //clear table
   for (let book of library) renderBook(book)
 }
 
@@ -67,14 +70,30 @@ function renderBook(book) {
   const newAuthor = document.createElement('td')
   const newPages = document.createElement('td')
   const newRead = document.createElement('td')
+  const del = document.createElement('td')
+
   newTitle.textContent = book.title
   newAuthor.textContent = book.author
   newPages.textContent = book.pages
   newRead.textContent = book.read
+
   newRow.appendChild(newTitle)
   newRow.appendChild(newAuthor)
   newRow.appendChild(newPages)
   newRow.appendChild(newRead)
+  newRow.appendChild(del)
+
+  const delBtn = document.createElement('button')
+  delBtn.textContent = "Delete"
+  delBtn.classList.add('delete')
+  delBtn.setAttribute('data-index', (library.indexOf(book)).toString())
+  del.appendChild(delBtn)
+  delBtn.addEventListener('click', (e) => deleteBook(parseInt(e.target.getAttribute('data-index'))))
+}
+
+function deleteBook(index) {
+  library.splice(index, 1)
+  renderTable(library)
 }
 
 /***  Testing   ****/
@@ -84,9 +103,10 @@ let hobbit = new Book("The Hobbit", "J. R. R. Tolkein", 295, false)
 let eden = new Book("East of Eden", "John Steinbeck", 512, true)
 let steppenwolf = new Book("Steppenwolf", "Herman Hesse", 254, true)
 let issueAtHand = new Book("The Issue at Hand", "Gil Fronsdal", 154, true)
-library.push(hobbit, eden, steppenwolf, issueAtHand)
+addBookToLibrary(hobbit)
+addBookToLibrary(eden)
+addBookToLibrary(steppenwolf)
+addBookToLibrary(issueAtHand)
 
 // test functions *****
-renderTable(library)
-
-
+// renderTable(library)
